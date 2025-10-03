3a5fea12f0cafe450e8fcab41a26cef7534530b8
Student <ajagekarketan01@gmail.com>
2025-10-03 14:43:37 +0530

    first

---
+++ functions.js
@@ -0,0 +1,6 @@
+function parameterizedFunction(name){
+    console.log('hello ${name}');
+    
+}
+
+parameterizedFunction("Ketan")
---
+++ index.js
@@ -0,0 +1,32 @@
+// let str = prompt("Enter the word")
+// let strArray = str.split("").reverse().join("")
+// str == strArray ? alert("It is palindrome"):alert("It is not palindrome")
+
+// for( i = 0;i<=10;i+=2){
+//    console.log(i);
+// }
+
+// sum=0
+// i=0
+// while(i<=10){
+//     sum+=i
+//     i++
+// }
+// console.log(i);
+
+// sum=1
+// i=1
+// while(i<=5){
+//     sum*=i
+//     i++
+// }
+// console.log(sum);
+
+
+let i=1;
+let fact=1;
+while (i<=5){
+    fact*=i;
+    i++
+}
+console.log(fact);
